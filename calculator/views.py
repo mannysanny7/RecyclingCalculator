065bf9d891fef88adfd551e1cd290835ed7f5313
mannysanny7 <25615439+mannysanny7@users.noreply.github.com>
2022-05-25 17:30:06 +0100

    Adding all folders

---
+++ calculator/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+	return render(request, 'calculator/home.html')
+
+def index(request):
+	return render(request, 'calculator/index.html')
