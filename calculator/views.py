54f8c54c7d0c15fe9b6a3afd2b57d90540addf36
mannysanny7 <25615439+mannysanny7@users.noreply.github.com>
2022-05-30 13:32:00 +0100

    deleted home file

--- calculator/views.py
+++ calculator/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-	return render(request, 'calculator/home.html')
-
 def index(request):
 	return render(request, 'calculator/index.html')
